58dbbb2ce77551879c4e89ca1867be5b6b6c93c8
Dru <lextand@gmail.com>
2024-03-11 18:43:38 +0300

    add command 'add_car'

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import threading
 import time
 
 from pytz import timezone
+from telebot import types
 
 
 logging.basicConfig(
@@ -19,6 +20,106 @@ schedule_logger = logging.getLogger('schedule')
 schedule_logger.setLevel(level=logging.DEBUG)
 
 bot = telebot.TeleBot(config.telegram_token)
+
+commands = ["Добавить ТС"]
+
+keyboard_main = types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_main.row(*[types.KeyboardButton(comm) for comm in commands])
+
+
+def add_car(message):
+    try:
+        inline_keys = []
+        with sq.connect(config.database) as con:
+            cur = con.cursor()
+            cur.execute("SELECT id, brand FROM car_brands")
+            result = cur.fetchall()
+        for record in result:
+            inline_keys.append(
+                types.InlineKeyboardButton(
+                    text=record[1],
+                    callback_data=f"add_car_brand {record[0]}"
+                )
+            )
+        inline_keys.append(
+            types.InlineKeyboardButton(
+                text="Новая марка",
+                callback_data="add_new_brand"
+            )
+        )
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(*inline_keys)
+        bot.send_message(
+            message.from_user.id,
+            text="Выбери марку",
+            reply_markup=keyboard
+        )
+    except Exception:
+        logging.critical("func add_car - error", exc_info=True)
+
+
+def add_car_model(message, call_data):
+    try:
+        brand_id = call_data.split()[0]
+        inline_keys = []
+        with sq.connect(config.database) as con:
+            cur = con.cursor()
+            cur.execute("SELECT id, model FROM car_models")
+            result = cur.fetchall()
+        for record in result:
+            inline_keys.append(
+                types.InlineKeyboardButton(
+                    text=record[1],
+                    callback_data=f"add_car_model {record[0]};{brand_id}"
+                )
+            )
+        inline_keys.append(
+            types.InlineKeyboardButton(
+                text="Новая модель",
+                callback_data="add_new_model"
+            )
+        )
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(*inline_keys)
+        bot.send_message(
+            message.from_user.id,
+            text="Выбери модель",
+            reply_markup=keyboard
+        )
+    except Exception:
+        logging.critical("func add_car_model - error", exc_info=True)
+
+
+def add_car_generation(message, call_data):
+    try:
+        brand_id, model_id = call_data.split()[0].split(";")
+        inline_keys = []
+        with sq.connect(config.database) as con:
+            cur = con.cursor()
+            cur.execute("SELECT id, generation FROM car_generations")
+            result = cur.fetchall()
+        for record in result:
+            inline_keys.append(
+                types.InlineKeyboardButton(
+                    text=record[1],
+                    callback_data=f"add_car_generation {record[0]};{brand_id};{model_id}"
+                )
+            )
+        inline_keys.append(
+            types.InlineKeyboardButton(
+                text="Новое поколение",
+                callback_data="add_new_generation"
+            )
+        )
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(*inline_keys)
+        bot.send_message(
+            message.from_user.id,
+            text="Выбери поколение",
+            reply_markup=keyboard
+        )
+    except Exception:
+        logging.critical("func add_car_generation - error", exc_info=True)
 
 
 def check_email(imap_server, email_login, email_password, teleg_id):
@@ -84,11 +185,21 @@ def schedule_main():
         time.sleep(1)
 
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if "add_car_brand" in call.data:
+        add_car_model(call.message, call.data)
+    elif "add_car_model" in call.data:
+        add_car_generation(call.message, call.data)
+
+
 @bot.message_handler(content_types=['text'])
 def take_text(message):
-    logging.warning(
-       f"func take_text: not understend question: {message.text}")
-    bot.send_message(message.chat.id, 'Я не понимаю, к сожалению')
+    if message.text.lower() == commands[0].lower():
+        add_car(message)
+    else:
+        logging.warning(f"func take_text: not understend question: {message.text}")
+        bot.send_message(message.chat.id, 'Я не понимаю, к сожалению')
 
 
 if __name__ == "__main__":
